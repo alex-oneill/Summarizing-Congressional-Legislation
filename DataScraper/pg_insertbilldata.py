bd1b2749303030f63d61ac50a38108911342f569
Alex <oneilla23@gmail.com>
2021-03-01 18:02:07 -0500

    Updated insert script to create and insert short_name field for full-bill info text

--- DataScraper/pg_insertbilldata.py
+++ DataScraper/pg_insertbilldata.py
@@ -21,6 +21,14 @@ def read_bill_info(file):
     return info_dict
 
 
+def make_short_name(info):
+    """Adjusts full-bill name and creates a short name. ZIP files must be removed from json prior."""
+    short_str = info['Title'].split('-')[1][:-4]
+    while short_str[-1].isalpha():
+        short_str = short_str[:-1]
+    return short_str
+
+
 params = config()
 conn = psycopg2.connect(**params)
 cur = conn.cursor()
@@ -40,9 +48,10 @@ cur = conn.cursor()
 # data = read_bill_info('SummaryBillData_117thCongress.json')
 # for k, v in data.items():
 #
-#     cur.execute("""INSERT INTO bill_info (full_title, name, section, link, congress, modified)
-#                 VALUES (%s, %s, %s, %s, %s, %s)""",
+#     cur.execute("""INSERT INTO bill_info (full_title, short_name, name, section, link, congress, modified)
+#                 VALUES (%s, %s, %s, %s, %s, %s, %s)""",
 #                 (v['Title'][:-4],
+#                  v['Title'].split('-')[1][:-4],
 #                  v['Title'].split('-')[1][:-4],
 #                  v['Section'],
 #                  v['Link'],
@@ -51,18 +60,20 @@ cur = conn.cursor()
 
 # INSERT FULL TEXT INFO INTO TABLE
 # todo adjust the input file
-data = read_bill_info('FullBillData_116thCongress.json')
-for k, v in data.items():
-
-    cur.execute("""INSERT INTO bill_info (full_title, name, section, link, congress, session, modified)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)""",
-                (v['Title'][:-4],
-                 v['Title'].split('-')[1][:-4],
-                 v['Section'],
-                 v['Link'],
-                 v['Congress'],
-                 v['Session'],
-                 v['Modified']))
+# data = read_bill_info('FullBillData_117thCongress.json')
+# for k, v in data.items():
+#
+#     short_name = make_short_name(v)
+#     cur.execute("""INSERT INTO bill_info (full_title, short_name, name, section, link, congress, session, modified)
+#                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
+#                 (v['Title'][:-4],
+#                  short_name,
+#                  v['Title'].split('-')[1][:-4],
+#                  v['Section'],
+#                  v['Link'],
+#                  v['Congress'],
+#                  v['Session'],
+#                  v['Modified']))
 
 cur.close()
 conn.commit()
