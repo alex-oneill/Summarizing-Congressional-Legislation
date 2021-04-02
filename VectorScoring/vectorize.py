e41e71d45d253da77ac22dc8320ae2d840fed4c7
Alex ONeill <73564404+alex-oneill@users.noreply.github.com>
2021-04-01 22:08:02 -0400

    Model epoch testing and pos/neg counters

--- VectorScoring/vectorize.py
+++ VectorScoring/vectorize.py
@@ -2,60 +2,107 @@ import random
 import gensim
 import smart_open
 import collections
-
-# import os
-# test_data_dir = os.path.join(gensim.__path__[0], 'test', 'test_data')
-# lee_train_file = os.path.join(test_data_dir, 'lee_background.cor')
-# lee_test_file = os.path.join(test_data_dir, 'lee.cor')
+import logging
 
 
 # SECTION: FUNCTIONS
 def read_corpus(fname, tokens_only=False):
     with smart_open.open(fname, encoding='iso-8859-1') as f:
         for i, line in enumerate(f):
-            tokens = gensim.utils.simple_preprocess(line)
-            if tokens_only:
-                yield tokens
+            if len(line.strip().split()) == 1:
+                logging.info('Line Dropped: {}'.format(line))
+                continue
             else:
-                yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
+                tokens = gensim.utils.simple_preprocess(line, min_len=3)
+                if tokens_only:
+                    yield tokens
+                else:
+                    yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
 
 
-# SECTION: MODEL
+# SECTION: MODEL PREP
+# FIXME: LOGGING IS OFF
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
 # NOTE: picked test doc has 10k+ lines
 train_corpus = list(read_corpus('corpus_docs\\116hr2500.cor'))
-# train_corpus = list(read_corpus(lee_train_file))
 
 # TODO: test and adjust model used for smaller docs
-model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=20)
-model.build_vocab(train_corpus)
+# SECTION: EPOCH TESTING
+for epoch in range(40, 120, 20):
 
-model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+    model = gensim.models.doc2vec.Doc2Vec(vector_size=24, min_count=5, epochs=epoch, dm=1, alpha=0.025,
+                                          min_alpha=0.020)
+    model.build_vocab(train_corpus)
+
+    model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+
+    # SECTION: MODEL EVALUATION
+    ranks = []
+    second_ranks = []
+    sim_hits = []
+    last_rank = []
+    # rand_docs = [random.randrange(0, len(train_corpus) - 1) for i in range(15)]
+    # rand_docs = [random.randrange(0, 100) for i in range(5)]
+    doc_scores = []
+    for doc_id in range(len(train_corpus)):
+    # for num in range(15):
+    #     doc_id = random.randint(0, len(train_corpus) - 1)
+        # NOTE: GETS VECTOR FOR DOC_ID WORDS
+        inferred_vector = model.infer_vector(train_corpus[doc_id].words, epochs=100)
+        # NOTE: MAKES LIST OF MOST SIMILAR VECTORS TO DOC id TUPLES (DOC-ID, SCORE)
+        sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+        # NOTE: STORES RANK OF ITSELF
+        rank = [docid for docid, sim in sims].index(doc_id)
+        ranks.append(rank)
+        second_ranks.append(sims[1])
+        last_rank.append(sims[-1])
+
+        # NOTE: GET POS/NEG VECTOR COUNTS
+        pos, neg = 0, 0
+        for doc in sims:
+            if doc[1] >= 0:
+                pos += 1
+            else:
+                neg += 1
+
+        doc_scores.append((doc_id, pos, neg))
+
+    # NOTE: PRINTS POS/NEG DOC COUNTS
+    # for doc in doc_scores:
+    #     print('\nDocument ({}): <<{}>>'.format(doc[0], ' '.join(train_corpus[doc[0]].words)))
+    #     print('POS: {}\tNEG: {}'.format(doc[1], doc[2]))
+
+    counter = collections.Counter(ranks)
+    v_lt_five = 0
+    total_docs = sum(counter.values())
+    for k, v in counter.items():
+        if k < 5:
+            v_lt_five += counter[k]
+    print('\nTop 5 Count: {}/{}: {}%'.format(v_lt_five, total_docs, (v_lt_five / total_docs) * 100))
+    print(counter[:15])
+    print(f'Epochs: {epoch}')
 
 
-# SECTION: MODEL EVALUATION
-ranks = []
-second_ranks = []
-# for doc_id in range(len(train_corpus)):
-for num in range(15):
-    doc_id = random.randint(0, len(train_corpus) - 1)
-    inferred_vector = model.infer_vector(train_corpus[doc_id].words)
-    sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
-    rank = [docid for docid, sim in sims].index(doc_id)
-    ranks.append(rank)
-    second_ranks.append(sims[1])
+# SECTION: RANDOM PRINTING FORMATS
+# NOTE: check random docs for similar and least-similar rows
+# if doc_id in rand_docs:
+#     sim_hits.append(sims)
+    # print('\nDocument ({}): «{}»'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
+    # print(u'\nSIMILAR/DISSIMILAR DOCS PER MODEL %s:' % model)
+    # for label, index in [('MOST SIMILAR', 0), ('SECOND-MOST SIMILAR', 1)]:
+    #     print(u'%s %s: «%s»' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
+    # for i in range(0, 4)[::-1]:
+    #     # for label, index in [('LEAST-{} SIMILAR'.format(i), len(sims) - i)]:
+    #     label, index = ('LEAST-{} SIMILAR'.format(i), len(sims) - i)
+    #     print(u'%s %s: «%s»' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
 
-    # NOTE: check random docs for similar and least-similar rows
-    print('\nDocument ({}): «{}»'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
-    for label, index in [('MOST SIMILAR', 0), ('SECOND-MOST SIMILAR', 1)]:
-        print(u'%s %s: «%s»' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
-    for i in range(1, 6)[::-1]:
-        for label, index in [('LEAST-{} SIMILAR'.format(i), len(sims) - i)]:
-            print(u'%s %s: «%s»' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
-
-    # NOTE: can use to see how many documents are most similar to themself. Will need to infer model for all
-    #   documents above then evaluate the counter output against the actual corpus size
-    # counter = collections.Counter(ranks)
-    # print(counter)
+# print('\nDocument ({}): «{}»'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
+# print(u'\nSIMILAR/DISSIMILAR DOCS PER MODEL %s:' % model)
+# for label, index in [('MOST SIMILAR', 0), ('SECOND-MOST SIMILAR', 1)]:
+#     print(u'%s %s: «%s»' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
+# NOTE: can use to see how many documents are most similar to themself. Will need to infer model for all
+#   documents above then evaluate the counter output against the actual corpus size
 
 # NOTE: default printing from doc2vec demo
 # print('Second-Most Similar Document {}: «{}»\n'.format(sim_id, ' '.join(train_corpus[sim_id[1][0]].words)))
