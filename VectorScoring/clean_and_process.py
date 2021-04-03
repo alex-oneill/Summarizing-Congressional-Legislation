6950ca21aa9a11a26d58e9fa2f667aeb9565c96e
Alex ONeill <73564404+alex-oneill@users.noreply.github.com>
2021-04-02 20:08:20 -0400

    update clean and process to load to db

--- VectorScoring/clean_and_process.py
+++ VectorScoring/clean_and_process.py
@@ -2,6 +2,7 @@ import psycopg2
 from configparser import ConfigParser
 import re
 import logging
+from gensim import corpora
 from Document import Document
 
 
@@ -64,17 +65,51 @@ def make_corpus(all_docs: list) -> list:
     return documents
 
 
+def write_corpus(docs: list) -> None:
+    """Writes documents to individual .cor files"""
+    for bill in docs:
+        title = bill.short_name
+        texts = [doc['stnd_row_text'] for doc in bill.texts]
+        file_name = title + '.cor'
+        with open('corpus_docs\\' + file_name, 'w') as file:
+            for sent in texts:
+                try:
+                    sent_line = ' '.join(sent)
+                    file.write(sent_line + '\n')
+                except UnicodeEncodeError as e:
+                    print('Unicode Error:\n', file_name, e)
+                    print(sent_line)
+
+
+def db_load(docs: list) -> None:
+    """Loads document items into Postgres database"""
+    for bill in docs:
+        title = bill.short_name
+        sum1, sum2, sum3 = bill.summary_1, bill.summary_2, bill.summary_3
+        for text in bill.texts:
+            blend_id = text['blend_id']
+            row_num = text['row_number']
+            row_text = text['row_text']
+            stnd_text = str(text['stnd_row_text'])
+
+            cur.execute("""INSERT INTO stnd_row_text (blend_id, row_number, row_text, stnd_text_list, short_name, 
+                        sum1, sum2, sum3) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
+                        (blend_id, row_num, row_text, stnd_text, title, sum1, sum2, sum3))
+            conn.commit()
+
+
 # SECTION: MAIN()
 params = config()
 conn = psycopg2.connect(**params)
 cur = conn.cursor()
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-text_rows = get_rows()
 # NOTE: text_rows tuple format...
 # 00 = {str} id # 01 = {str} blend_id # 02 = {str} short_name # 03 = {str} name # 04 = {str} full_title
 # 05 = {str} short_title # 06 = {str} summary_1 # 07 = {str} summary_2 # 08 = {str} summary_3
 # 09 = {int} row_number # 10 = {str} row_text
+
+text_rows = get_rows()
 
 stnd_rows = []
 for row in text_rows:
@@ -83,7 +118,9 @@ for row in text_rows:
 
 doc_list = make_corpus(stnd_rows)
 
-# NOTE: inspect document class objects
+db_load(doc_list)
+
+# NOTE: INSPECT DOCUMENT OBJECT
 # for bill in doc_list[:1]:
 #     texts = [doc['stnd_row_text'] for doc in bill.texts]
 #     corp_dictionary = corpora.Dictionary(texts)
@@ -92,21 +129,13 @@ doc_list = make_corpus(stnd_rows)
 #     corpus = [corp_dictionary.doc2bow(text) for text in texts]
 #     print(corpus)
 
-# NOTE: write docs to text corpus .cor files
+# NOTE: GET BILLS OVER 500 ROWS
 # for bill in doc_list:
-#     title = bill.short_name
-#     texts = [doc['stnd_row_text'] for doc in bill.texts]
-#     file_name = title + '.cor'
-#     with open('corpus_docs\\' + file_name, 'w') as file:
-#         for sent in texts:
-#             try:
-#                 sent_line = ' '.join(sent)
-#                 file.write(sent_line + '\n')
-#             except UnicodeEncodeError as e:
-#                 print('Unicode Error:\n', file_name, e)
-#                 print(sent_line)
+#     size = len(bill.texts)
+#     if size >= 500:
+#         print(size)
 
-# NOTE: reclaim memory
+# NOTE: RECLAIM MEMORY
 # del text_rows
 
 cur.close()
